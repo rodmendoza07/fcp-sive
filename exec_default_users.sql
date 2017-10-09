434b1d5cb74e51e25adc88640ba7f8661f157ee5
rodrigo <luis.mendoza@grupoporvenir.com.mx>
2017-10-09 18:06:19 -0500

    bloqueo de usuarios

---
+++ exec_default_users.sql
@@ -0,0 +1,21 @@
+USE CATALOGOS
+
+BEGIN TRAN
+	BEGIN TRY
+		EXEC CATALOGOS.dbo.sp_BlockUser 334, 'Requerimiento Gonzalo Vargas', 11, 'eduahc'
+		EXEC CATALOGOS.dbo.sp_BlockUser 969, 'Requerimiento Gonzalo Vargas', 11, 'eduahc'
+		EXEC CATALOGOS.dbo.sp_BlockUser 796, 'Requerimiento Gonzalo Vargas', 11, 'eduahc'
+
+	IF @@TRANCOUNT> 0
+		Commit tran
+
+ END TRY
+BEGIN CATCH
+	IF @@TRANCOUNT> 0
+		BEGIN
+			rollback tran
+			SELECT   
+        ERROR_NUMBER() AS ErrorNumber  
+       ,ERROR_MESSAGE() AS ErrorMessage;  
+		 END
+END CATCH
---
+++ vw_BlockedUsers.sql
@@ -0,0 +1,32 @@
+USE [CATALOGOS]
+GO
+
+/****** Object:  View [dbo].[vw_BlockedUsers]    Script Date: 06/10/2017 11:27:06 a. m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE View [dbo].[vw_BlockedUsers]
+AS
+	SELECT bl.id as id_BlockedUser,
+		emp.id_empleados,
+		bl.blocked_by as blocked_by_id,
+		emp.cve_puesto,
+		emp.Nombre,
+		emp.ap_paterno,
+		emp.ap_materno,
+		emp.usuario,
+		job.descripcion,
+		dpto.descripcion as Departamento,
+		bl.motivo,
+		bl.date_block as 'Fecha de bloqueo'
+	FROM CATALOGOS.dbo.tc_blockedUsers bl
+	INNER JOIN CATALOGOS.dbo.tc_empleados emp ON bl.empleados_id = emp.id_empleados
+	INNER JOIN CATALOGOS.dbo.tc_departamento dpto on emp.cve_depto = dpto.id_departamento
+	INNER JOIN CATALOGOS.dbo.tc_puesto job on emp.cve_puesto = job.id_puesto
+GO
+
+
